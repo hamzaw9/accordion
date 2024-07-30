848067f0cadb2805b49fcf7d8d605ad1fd47d478
Hamza Baig <mail.hamzabaig@gmail.com>
2024-07-30 17:02:52 +0500

    Implement single-selection functionality for Accordion component

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,16 +1,30 @@
+import { useState } from 'react';
 import data from './data';
 
 const Accordion = () => {
+  const [selected, setSelected] = useState(null);
+
+  const handleSingleSelection = (currentid) => {
+    setSelected(selected === currentid ? null : currentid);
+  };
+
   return (
     <div className="wrapper">
       <div className="accordion">
         {data && data.length ? (
           data.map((dataItem) => (
-            <div className="item">
+            <div
+              onClick={() => handleSingleSelection(dataItem.id)}
+              className="item"
+              key={dataItem.id}
+            >
               <div className="title">
                 <h3>{dataItem.title}</h3>
                 <span>+</span>
               </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.content}</div>
+              ) : null}
             </div>
           ))
         ) : (
