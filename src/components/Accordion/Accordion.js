cf5b2d3a028943d7a33f3745c52d5d9da61963a1
Hamza Baig <mail.hamzabaig@gmail.com>
2024-07-30 16:53:35 +0500

    Implement basic Accordion component with data mapping

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,4 +1,23 @@
+import data from './data';
+
 const Accordion = () => {
-  return <div>Accordion</div>;
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {data && data.length ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div className="title">
+                <h3>{dataItem.title}</h3>
+                <span>+</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
 };
 export default Accordion;
