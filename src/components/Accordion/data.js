7bcc2dabedd23aabfc270bc208199c8e3341254a
Hamza Baig <mail.hamzabaig@gmail.com>
2024-07-30 16:46:41 +0500

    Set up Accordion component with initial structure and dummy data

---
+++ src/components/Accordion/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    title: 'Accordion Item #1',
+    content:
+      'This is the content for the first accordion item. It contains detailed information.',
+  },
+  {
+    id: 2,
+    title: 'Accordion Item #2',
+    content:
+      'This is the content for the second accordion item. It provides insights and details.',
+  },
+  {
+    id: 3,
+    title: 'Accordion Item #3',
+    content:
+      'This is the content for the third accordion item. Here, you can find more information.',
+  },
+  {
+    id: 4,
+    title: 'Accordion Item #4',
+    content:
+      'This is the content for the fourth accordion item. Here, you can find fourth topic.',
+  },
+];
+
+export default data;
